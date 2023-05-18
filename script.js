f48a0db531cd3a7d3923a0a650d68aec29190946
Rahil Habibli <rahilah@code.edu.az>
2023-05-18 12:48:43 +0400

    datetime added

--- script.js
+++ script.js
@@ -6,17 +6,24 @@ fetch(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=Baku&aqi=no`)
 const container = document.querySelector(".container");
 const countryCitySpan = document.querySelector(".countryCitySpan");
 const localTimeSpan = document.querySelector(".localTimeSpan");
+
 function renderWeather(weather) {
   //Country name / City
   const countryName = weather.location.country;
   const cityName = weather.location.name;
   countryCitySpan.innerText = `${countryName},${cityName}`;
 
-  //Local time in "hh:mm"
-  const localTime = weather.location.localTime;
-  const formattedLocalTime = new Date(localTime).toLocaleTimeString([], {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-  localTimeSpan.innerText = `${formattedLocalTime}`;
+  const localTime = weather.location.localtime;
+  const formattedTime = localTime.slice(11, 16).trim();
+  console.log(formattedTime);
 }
+// // Split the datetime string into date and time parts
+// const [datePart, timePart] = localTime.split(" ");
+
+// // Extract the hour and minute from the time part
+// const [hour, minute] = timePart.split(":");
+
+// // Create the formatted time string
+// const formattedTime = `${hour}:${minute}`;
+
+// console.log(formattedTime);
