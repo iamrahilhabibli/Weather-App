c5f523c147c68cffe75b5287d46c8533503e86c4
Habibli Rahil <RahilaH@code.local>
2023-05-18 15:14:16 +0400

    api data linked

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ fetch(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=Baku&aqi=no`)
   .then((x) => x.json())
   .then((x) => renderWeather(x));
 
+fetch(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=Baku&aqi=no`)
+  .then((x) => x.json())
+  .then((x) => console.log(x));
+
 const container = document.querySelector(".container");
 const countryCitySpan = document.querySelector(".countryCitySpan");
 const localTimeSpan = document.querySelector(".localTimeSpan");
 const imgContainer = document.querySelector(".imgContainer");
+const tempCSpan = document.querySelector(".tempCSpan");
+const windSSpan = document.querySelector(".windSSpan");
+const humiditySpan = document.querySelector(".humiditySpan");
+const feelsLikeSpan = document.querySelector(".feelsLikeSpan");
+const uvIndexSpan = document.querySelector(".uvIndexSpan");
 
 function renderWeather(weather) {
   //Country name / City
@@ -24,8 +33,25 @@ function renderWeather(weather) {
   const iconForCondition = weather.current.condition.icon;
   const icon = document.createElement("img");
   imgContainer.appendChild(icon);
-  const lenghtOfPath = iconForCondition.length;
-  const formattedIconPath = iconForCondition.slice(2, lenghtOfPath);
-  console.log(formattedIconPath);
-  icon.setAttribute("src", formattedIconPath);
+  // const lenghtOfPath = iconForCondition.length;
+  // const formattedIconPath = iconForCondition.slice(2, lenghtOfPath);
+  console.log(iconForCondition);
+  icon.setAttribute("src", iconForCondition);
+
+  //Temp
+  const currentTemp = weather.current.temp_c;
+  tempCSpan.innerText = currentTemp + " °C";
+
+  //Wind speed....
+  const windSpeed = weather.current.wind_kph;
+  windSSpan.innerText = windSpeed + " km/h";
+  //Humidity
+  const humidity = weather.current.humidity;
+  humiditySpan.innerText = humidity + " %";
+
+  const feelsLikeC = weather.current.temp_c;
+  feelsLikeSpan.innerText = feelsLikeC + " °C";
+
+  const uvIndex = weather.current.uv;
+  uvIndexSpan.innerText = uvIndex;
 }
