66eb413e367767a62f7bcdc1b05f3909c33511d4
Habibli Rahil <RahilaH@code.local>
2023-05-18 18:03:15 +0400

    dynamic js added

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ fetch(
 //   .then((x) => x.json())
 //   .then((x) => console.log(x));
 
-const container = document.querySelector(".container");
 const countryCitySpan = document.querySelector(".countryCitySpan");
 const localTimeSpan = document.querySelector(".localTimeSpan");
 const imgContainer = document.querySelector(".imgContainer");
@@ -27,38 +26,99 @@ const searchBar = document.querySelector("#search-bar");
 const searchBtn = document.querySelector("#search-btn");
 
 function renderWeather(weather) {
-  //Country name / City
+  const container = document.createElement("div");
+  container.classList.add("container");
+
+  const topSpanContainer = document.createElement("div");
+  topSpanContainer.classList.add("topSpanContainer");
+  container.appendChild(topSpanContainer);
+
+  const countryCitySpan = document.createElement("span");
+  countryCitySpan.classList.add("countryCitySpan");
+  topSpanContainer.appendChild(countryCitySpan);
+
   const countryName = weather.location.country;
   const cityName = weather.location.name;
   countryCitySpan.innerText = `${countryName},${cityName}`;
 
-  //Local Time
+  const localTimeSpan = document.createElement("span");
+  localTimeSpan.classList.add("localTimeSpan");
+  topSpanContainer.appendChild(localTimeSpan);
   const localTime = weather.location.localtime;
   const formattedTime = localTime.slice(11, 16);
-
   localTimeSpan.innerText = formattedTime;
 
-  //icon
+  const currentDateSpan = document.createElement("span");
+  currentDateSpan.classList.add("currentDateSpan");
+  currentDateSpan.textContent = "Thursday, 18th May";
+  topSpanContainer.appendChild(currentDateSpan);
+
+  const imgContainer = document.createElement("div");
+  imgContainer.classList.add("imgContainer");
+  container.appendChild(imgContainer);
   const iconForCondition = weather.current.condition.icon;
   const icon = document.createElement("img");
   icon.src = "https:" + iconForCondition;
   imgContainer.appendChild(icon);
-  //Temp
+
+  const midSpanContainer = document.createElement("div");
+  midSpanContainer.classList.add("midSpanContainer");
+  container.appendChild(midSpanContainer);
+
+  const midSpanTop = document.createElement("div");
+  midSpanTop.classList.add("midSpanTop");
+  midSpanContainer.appendChild(midSpanTop);
+
+  const tempCSpan = document.createElement("span");
+  tempCSpan.classList.add("tempCSpan");
+  midSpanContainer.appendChild(tempCSpan);
   const currentTemp = weather.current.temp_c;
   tempCSpan.innerText = currentTemp + " °C";
 
-  //Wind speed....
+  const midSpanOther = document.createElement("div");
+  midSpanOther.classList.add("midSpanOther");
+  midSpanContainer.appendChild(midSpanOther);
+
+  const windSSpan = document.createElement("span");
+  windSSpan.classList.add("windSSpan");
+  midSpanOther.appendChild(windSSpan);
   const windSpeed = weather.current.wind_kph;
   windSSpan.innerText = windSpeed + " km/h";
-  //Humidity
+
+  const humiditySpan = document.createElement("span");
+  humiditySpan.classList.add("humiditySpan");
+  midSpanOther.appendChild(humiditySpan);
   const humidity = weather.current.humidity;
   humiditySpan.innerText = humidity + " %";
 
+  const feelsLikeSpan = document.createElement("span");
+  feelsLikeSpan.classList.add("feelsLikeSpan");
+  midSpanOther.appendChild(feelsLikeSpan);
   const feelsLikeC = weather.current.temp_c;
   feelsLikeSpan.innerText = feelsLikeC + " °C";
 
+  const uvIndexSpan = document.createElement("span");
+  uvIndexSpan.classList.add("uvIndexSpan");
+  midSpanOther.appendChild(uvIndexSpan);
   const uvIndex = weather.current.uv;
   uvIndexSpan.innerText = uvIndex;
+
+  const botSearchContainer = document.createElement("div");
+  botSearchContainer.classList.add("botSearchContainer");
+  container.appendChild(botSearchContainer);
+
+  const searchInput = document.createElement("input");
+  searchInput.setAttribute("type", "text");
+  searchInput.setAttribute("placeholder", "Enter a location");
+  searchInput.setAttribute("id", "search-bar");
+  botSearchContainer.appendChild(searchInput);
+
+  const searchButton = document.createElement("button");
+  searchButton.setAttribute("id", "search-btn");
+  searchButton.textContent = "Search";
+  botSearchContainer.appendChild(searchButton);
+
+  document.body.appendChild(container);
 }
 
 // searchBar.addEventListener("keyup", (e) => {
@@ -70,94 +130,3 @@ function renderWeather(weather) {
 //   getWeather();
 //   console.log(initialSearch);
 // });
-
-// // Create the main container element
-// var container = document.createElement("div");
-// container.classList.add("container");
-
-// // Create the top span container
-// var topSpanContainer = document.createElement("div");
-// topSpanContainer.classList.add("topSpanContainer");
-// container.appendChild(topSpanContainer);
-
-// // Create the country city span
-// var countryCitySpan = document.createElement("span");
-// countryCitySpan.classList.add("countryCitySpan");
-// topSpanContainer.appendChild(countryCitySpan);
-
-// // Create the local time span
-// var localTimeSpan = document.createElement("span");
-// localTimeSpan.classList.add("localTimeSpan");
-// topSpanContainer.appendChild(localTimeSpan);
-
-// // Create the current date span
-// var currentDateSpan = document.createElement("span");
-// currentDateSpan.classList.add("currentDateSpan");
-// currentDateSpan.textContent = "Thursday, 18th May";
-// topSpanContainer.appendChild(currentDateSpan);
-
-// // Create the image container
-// var imgContainer = document.createElement("div");
-// imgContainer.classList.add("imgContainer");
-// container.appendChild(imgContainer);
-
-// // Create the mid span container
-// var midSpanContainer = document.createElement("div");
-// midSpanContainer.classList.add("midSpanContainer");
-// container.appendChild(midSpanContainer);
-
-// // Create the mid span top
-// var midSpanTop = document.createElement("div");
-// midSpanTop.classList.add("midSpanTop");
-// midSpanContainer.appendChild(midSpanTop);
-
-// // Create the temperature span
-// var tempCSpan = document.createElement("span");
-// tempCSpan.classList.add("tempCSpan");
-// midSpanContainer.appendChild(tempCSpan);
-
-// // Create the mid span other
-// var midSpanOther = document.createElement("div");
-// midSpanOther.classList.add("midSpanOther");
-// midSpanContainer.appendChild(midSpanOther);
-
-// // Create the wind span
-// var windSSpan = document.createElement("span");
-// windSSpan.classList.add("windSSpan");
-// midSpanOther.appendChild(windSSpan);
-
-// // Create the humidity span
-// var humiditySpan = document.createElement("span");
-// humiditySpan.classList.add("humiditySpan");
-// midSpanOther.appendChild(humiditySpan);
-
-// // Create the feels-like span
-// var feelsLikeSpan = document.createElement("span");
-// feelsLikeSpan.classList.add("feelsLikeSpan");
-// midSpanOther.appendChild(feelsLikeSpan);
-
-// // Create the UV index span
-// var uvIndexSpan = document.createElement("span");
-// uvIndexSpan.classList.add("uvIndexSpan");
-// midSpanOther.appendChild(uvIndexSpan);
-
-// // Create the bottom search container
-// var botSearchContainer = document.createElement("div");
-// botSearchContainer.classList.add("botSearchContainer");
-// container.appendChild(botSearchContainer);
-
-// // Create the search input
-// var searchInput = document.createElement("input");
-// searchInput.setAttribute("type", "text");
-// searchInput.setAttribute("placeholder", "Enter a location");
-// searchInput.setAttribute("id", "search-bar");
-// botSearchContainer.appendChild(searchInput);
-
-// // Create the search button
-// var searchButton = document.createElement("button");
-// searchButton.setAttribute("id", "search-btn");
-// searchButton.textContent = "Search";
-// botSearchContainer.appendChild(searchButton);
-
-// // Append the container to the document body or any desired parent element
-// document.body.appendChild(container);
