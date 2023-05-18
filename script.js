fad145dfb90edb6bf2a41f09a80efed0d2bdcee3
Rahil Habibli <rahilah@code.edu.az>
2023-05-18 12:05:57 +0400

    design testing

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const api_key = "7e58c48abd6d40fe85b54302231805";
-fetch(
-  `http://api.weatherapi.com/v1/current.json?key=${api_key}&q=London&aqi=no`
-)
+fetch(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=Baku&aqi=no`)
   .then((x) => x.json())
-  .then((x) => console.log(x));
+  .then((x) => renderWeather(x));
+
+const container = document.querySelector(".container");
+
+// function renderWeather(weather) {
+//   const tempCel = weather.current.temp_c;
+//   const windKph = weather.current.wind_kph;
+//   const visKm = weather.current.vis_km;
+
+//   const tempSpan = document.createElement("span");
+//   tempSpan.innerText = `Temprature is: ${tempCel}`;
+
+//   const windSpan = document.createElement("span");
+//   windSpan.innerText = `Temprature is: ${windKph}`;
+
+//   const visSpan = document.createElement("span");
+//   visSpan.innerText = `Temprature is: ${visKm}`;
+
+//   container.appendChild(tempSpan);
+//   container.appendChild(windSpan);
+//   container.appendChild(visSpan);
+// }
