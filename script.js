21fae91c9f25867468157711ff2f69231a6b8be7
Habibli Rahil <RahilaH@code.local>
2023-05-18 13:40:42 +0400

    img container started

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ fetch(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=Baku&aqi=no`)
 const container = document.querySelector(".container");
 const countryCitySpan = document.querySelector(".countryCitySpan");
 const localTimeSpan = document.querySelector(".localTimeSpan");
+const imgContainer = document.querySelector(".imgContainer");
 
 function renderWeather(weather) {
   //Country name / City
@@ -13,17 +14,11 @@ function renderWeather(weather) {
   const cityName = weather.location.name;
   countryCitySpan.innerText = `${countryName},${cityName}`;
 
+  //Local Time
   const localTime = weather.location.localtime;
-  const formattedTime = localTime.slice(11, 16).trim();
+  const formattedTime = localTime.slice(11, 16);
   console.log(formattedTime);
+  localTimeSpan.innerText = formattedTime;
+
+  //icon
 }
-// // Split the datetime string into date and time parts
-// const [datePart, timePart] = localTime.split(" ");
-
-// // Extract the hour and minute from the time part
-// const [hour, minute] = timePart.split(":");
-
-// // Create the formatted time string
-// const formattedTime = `${hour}:${minute}`;
-
-// console.log(formattedTime);
